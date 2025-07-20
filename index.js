25b7ebc7b9f6680e7bc2295f66b6ccad9f606cc3
Dan Martin <codanmartin.25@gmail.com>
2025-07-21 00:08:30 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,100 @@
+const input = require('sync-input');
+
+/**
+ * Default product list for the shop.
+ * @type {Array<{ name: string, price: number, earnings: number}>}
+ */
+const defaultProducts = [
+    {name: "Bubblegum", price: 2, earnings: 202},
+    {name: "Toffee", price: 0.2, earnings: 118},
+    {name: "Ice cream", price: 5, earnings: 2250},
+    {name: "Milk chocolate", price: 4, earnings: 1680},
+    {name: "Doughnut", price: 2.5, earnings: 1075},
+    {name: "Pancake", price: 3.2, earnings: 80}
+];
+
+/**
+ * Represents a corner shop with a list of products.
+ */
+class CornerShop {
+    /**
+     * Creates a new instance of CornerShop.
+     * @param {Array<{ name: string, price: number, earnings: number}>} products
+     */
+    constructor(products = []) {
+        this.products = products;
+        this.staffExpenses = 0
+        this.otherExpenses = 0
+        this.netIncome = 0
+        this.earnings = products.reduce((acc, cur) => {
+            return acc + cur.earnings
+        }, 0);
+    }
+
+    /**
+     * Prints the name and price of each product.
+     * @returns {void}
+     */
+    printPrices() {
+        console.log("Prices:");
+        this.products.forEach(product => {
+            console.log(`${product.name}: $${product.price}`);
+        });
+    }
+
+    /**
+     * Prints the name and earnings of each product and the total income.
+     * @returns {void}
+     */
+    printEarnings() {
+        console.log("Earned amount:");
+        this.products.forEach(product => {
+            console.log(`${product.name}: $${product.earnings}`);
+        })
+
+        console.log(`\nIncome: $${this.earnings}`)
+    }
+
+    /**
+     * Prints the name and earnings of each product and the total income.
+     * @returns {void}
+     */
+    printNetIncome() {
+        console.log(`Net income: $${this.netIncome}`);
+    }
+
+    /**
+     * Recalculates the net income of the shop
+     * @returns {void}
+     */
+    updateNetIncome() {
+        this.netIncome = this.earnings - this.staffExpenses - this.otherExpenses
+    }
+
+    /**
+     * Prompts the user to input inside the terminal staff expenses.
+     * @returns {void}
+     */
+    gatherStaffExpenses() {
+        this.staffExpenses = Number(input("Staff expenses: "))
+    }
+
+    /**
+     * Prompts the user to input inside the terminal other expenses.
+     * @returns {void}
+     */
+    gatherOtherExpenses() {
+        this.otherExpenses = Number(input("Other expenses: "))
+    }
+
+
+}
+
+const myCornerShop = new CornerShop(defaultProducts);
+
+// myCornerShop.printPrices();
+myCornerShop.printEarnings();
+myCornerShop.gatherStaffExpenses();
+myCornerShop.gatherOtherExpenses();
+myCornerShop.updateNetIncome();
+myCornerShop.printNetIncome();
---
+++ readme.md
@@ -0,0 +1,22 @@
+# My First Project (JavaScript)
+
+Imagine that you're the owner of a new small corner shop. You are doing well and want to determine how much you earned in the first month. Create a simple tool that will calculate your net income.
+Upon completing this project, you can use this tool for your own accounting needs.
+
+---
+
+## What you'll learn
+
+Once you choose a project, we’ll provide you with a study plan that includes all the necessary topics from your course to get it built. Here’s what awaits you:
+
+### ▶️ Stage 1/3: Print the prices
+
+Start with printing the names of items and their prices using your first JavaScript function.
+
+### ▶️ Stage 2/3: Measure the total income of your shop
+
+You know how much you earned by selling each item. Calculate the total earnings before staff and other expenses!
+
+### ▶️ Stage 3/3: Calculate net income
+
+Measure the shop’s net income. Subtract staff, electricity, and security expenses.
